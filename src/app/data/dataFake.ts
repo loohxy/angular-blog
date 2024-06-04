8a738d63e1678cdc724e0dc8953443bcdeeb2b07
lorena <lorenafernandescarvalho536@gmail.com>
2024-06-04 15:29:34 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"SAILOR MOON GANHA NOVOS EPISÓDIOS",
+    "description":"O ANIME GANHARÁ 30 NOVOS EPISÓDIOS",
+    "photoCover":"https://s.yimg.com/ny/api/res/1.2/V6L917VVxz2uxSfS6Uc2kA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MQ--/https://media.zenfs.com/en-US/homerun/hello_giggles_454/cc8d778fcc1e41e69782e150b2f500a9"
+
+  },
+  {
+    "id":"2",
+    "title":"jujutsu-kaisen já tem data de estreia confirmada",
+    "description":"DIRETOR DO ANIME JUJUTSU-KAISEN FALA SOBRE A NOVA TEMPORADA",
+    "photoCover":"https://images.augustman.com/wp-content/uploads/sites/2/2023/07/07151915/jujutsu-kaisen-3.jpg"
+  }
+
+]
